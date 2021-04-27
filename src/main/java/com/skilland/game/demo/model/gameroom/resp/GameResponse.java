94ddc65a69e6c48791df3c45948bccad37cf8072
soniabond <soniabondarenko1@gmail.com>
2021-04-27 16:27:25 +0300

    game creation and ability to get all games avaliable for user added

---
+++ src/main/java/com/skilland/game/demo/model/gameroom/resp/GameResponse.java
@@ -0,0 +1,23 @@
+package com.skilland.game.demo.model.gameroom.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameResponse {
+
+    private long gameId;
+    private String gameName;
+    private String subjectName;
+    private String [] topicNames;
+    private int minTaskLevel;
+    private int maxTaskLevel;
+    private int durabilityMinutes;
+}
